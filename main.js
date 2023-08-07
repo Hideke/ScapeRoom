97181f9da98969c44b8080dcee8e73e971e7d339
Caique <hidekehyuuga@gmail.com>
2023-08-06 23:47:05 -0300

    PatchV0.4
    + fases (até a 13);
    Inclusão de códigos e cifras;
    Titulo da pagina arrumado.

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ btn.addEventListener("click", function(){
 
     var erro = document.getElementById("error");
 
+    var bait1 = document.getElementById("bait-1");
+    var bait2 = document.getElementById("bait-2");
+
     var um = document.getElementById("um");
     var dois = document.getElementById("dois");
     var tres = document.getElementById("tres");
@@ -31,7 +34,10 @@ btn.addEventListener("click", function(){
     var oito = document.getElementById("oito");
     var nove = document.getElementById("nove");
     var dez = document.getElementById("dez");
-
+    var onze = document.getElementById("onze");
+    var doze = document.getElementById("doze");
+    var treze = document.getElementById("treze");
+treze
     switch (resultado) {
         default:
             audio.play();
@@ -135,7 +141,89 @@ btn.addEventListener("click", function(){
             sete.style.display = "none";
             oito.style.display = "none";
             nove.style.display = "none";
-            dez.style.display = "block";
+            setTimeout(() => {
+                texto.value = '';
+                bait1.style.display = "block";
+            }, 100);
+            setTimeout(() => {
+                bait1.style.display = "none";
+                bait2.style.display = "block";
+            }, 4000);
+            setTimeout(() => {
+                bait2.style.display = "none";
+                dez.style.display = "block";
+            }, 7000);
+            break;
+        /* Resposta Fase 10 */
+        case "trazendoosquestionamentosdaescrita":
+        case "trazendo os questionamentos da escrita":
+            texto.value = '';
+            um.style.display = "none";
+            dois.style.display = "none";
+            tres.style.display = "none";
+            quatro.style.display = "none";
+            cinco.style.display = "none";
+            seis.style.display = "none";
+            sete.style.display = "none";
+            oito.style.display = "none";
+            nove.style.display = "none";
+            dez.style.display = "none";
+            onze.style.display = "block";
+            break;
+        /* Resposta Fase 11 */
+        case "trouxemosalinguagemaomundo,":
+        case "trouxemos a linguagem ao mundo,":
+            texto.value = '';
+            um.style.display = "none";
+            dois.style.display = "none";
+            tres.style.display = "none";
+            quatro.style.display = "none";
+            cinco.style.display = "none";
+            seis.style.display = "none";
+            sete.style.display = "none";
+            oito.style.display = "none";
+            nove.style.display = "none";
+            dez.style.display = "none";
+            onze.style.display = "none";
+            doze.style.display = "block";
+            break;
+        /* Resposta Fase 12 */
+        case "cuneiforme que ecoam ainda hoje.":
+        case "cuneiformequeecoamaindahoje.":
+            texto.value = '';
+            um.style.display = "none";
+            dois.style.display = "none";
+            tres.style.display = "none";
+            quatro.style.display = "none";
+            cinco.style.display = "none";
+            seis.style.display = "none";
+            sete.style.display = "none";
+            oito.style.display = "none";
+            nove.style.display = "none";
+            dez.style.display = "none";
+            onze.style.display = "none";
+            doze.style.display = "none";
+            treze.style.display = "block";
+            break;
+        /* Resposta Fase 13 */
+        case "nas tramas das tábuas antigas,":
+        case "nas tramas das tabuas antigas,":
+        case "nastramasdastábuasantigas,":
+        case "nastramasdastabuasantigas,":
+            texto.value = '';
+            um.style.display = "none";
+            dois.style.display = "none";
+            tres.style.display = "none";
+            quatro.style.display = "none";
+            cinco.style.display = "none";
+            seis.style.display = "none";
+            sete.style.display = "none";
+            oito.style.display = "none";
+            nove.style.display = "none";
+            dez.style.display = "none";
+            onze.style.display = "none";
+            doze.style.display = "none";
+            treze.style.display = "none";
             break;
     }
 
